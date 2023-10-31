e9c0916c65f212f949888747f83771dcbb9cbe92
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-10-30 20:32:34 -0500

    1

---
+++ seeders/20231031010834-bands.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Bands',[
+    {
+      name: 'Black Sabbath',
+      genre: 'metal',
+      available_start_time: ' 1970-01-01',
+      end_time: '2020-01-01',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
